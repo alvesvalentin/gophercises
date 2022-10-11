7d188d10b9d71500f579e3770c9ab36e3de041e5
Valentin Alvès <valentin-alves@hotmail.com>
2022-10-11 15:44:49 +0200

    finished exercise

--- quiz_game/01_quigame.go
+++ quiz_game/01_quigame.go
@@ -1,10 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	exercises()
 }
 func exercises() {
-	fmt.Println("hello world")
+	fmt.Println("hello world !")
+	s := flag.String("csv-file", "problems.csv", "CSV file flag")
+
+	flag.Parse()
+
+	fmt.Println(*s)
+
+	file, err := os.Open("quiz_game/" + *s)
+	if err != nil {
+		log.Fatalf("error opening %s", *s)
+	}
+
+	reader := csv.NewReader(file)
+
+	all, err := reader.ReadAll()
+	var correctAnswers int
+
+	for _, val := range all {
+		var input string
+		fmt.Printf("Question: %s... \n", val[0])
+
+		fmt.Scanln(&input)
+		if input == val[1] {
+			fmt.Println("Correct !")
+			correctAnswers++
+		} else {
+			fmt.Println("Incorrect !")
+		}
+	}
+
+	fmt.Printf("You have %d/%d correct answers ! \n", correctAnswers, len(all))
+
 }
